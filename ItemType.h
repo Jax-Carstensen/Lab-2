acfd4bd4496da82279c91aa401b50760a2182248
Jax-Carstensen <39423374+Jax-Carstensen@users.noreply.github.com>
2021-02-01 18:44:10 -0600

    Update ItemType.h

--- ItemType.h
+++ ItemType.h
@@ -10,10 +10,11 @@ enum RelationType {
 
 class ItemType {
 private:
-	type value = 0;
+	type value;
 public:
-	ItemType() {}
+	ItemType() { value = 0; }
 	ItemType(type newValue) { value = newValue; }
+
 	RelationType ComparedTo(ItemType otherItem) {
 		if (value > otherItem.getValue())
 			return GREATER;
@@ -21,7 +22,15 @@ public:
 			return LESSER;
 		return EQUAL;
 	}
+	//Pre:  An ItemType to compare to
+	//Post: GREATER if this ItemType's value is higher, LESSER if it is lesser, and EQUAL if the values are the same
+
 	void setValue(type newValue) { value = newValue; }
+	//Pre:  An int (in this case) to set this item's value to
+	//Post: Sets this item's value to the provided one
+
 	type getValue() { return value; }
+	//Pre:  None
+	//Post: Returns this item's value
 };
 #endif

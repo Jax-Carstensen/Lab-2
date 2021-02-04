940ad226a26ef901f78915067c5a2c4c78f2b868
Jax-Carstensen <39423374+Jax-Carstensen@users.noreply.github.com>
2021-02-03 21:22:39 -0600

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,3 +1,15 @@
+ /*
+	Name:         Jax Carstensen
+	Semester:     2
+	Due:          02/03/2021
+	Lab:          Lab 2 (Unsorted Linked List)
+	Description:  A data structure of objects arranged in a linear order. The order is determined by a pointer in each object. 
+	See definition on pg. 134 and implementation on pg. 167 of the 6th edition of C++ Plus Data Structures. Your welcome to 
+	learn from the books implementation, however, make sure to write your own code. This unsorted list must be implemented 
+	with pointers. You will then write a client code that will have a function that will take two unsorted lists and perform 
+	the union set operation. The resulting new unsorted list will consist of all the unique items from the original two lists 
+	with no duplicates.
+*/
 #include <iostream>
 #include "LinkedList.h"
 
